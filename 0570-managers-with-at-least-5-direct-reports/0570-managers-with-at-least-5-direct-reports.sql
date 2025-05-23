9c52b40b0bd18c19863d48aabd677eac698b88de
Lakshmi Siva Karthik Padala <siva711karthik@gmail.com>
2025-05-23 07:54:04 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -2,9 +2,8 @@
 SELECT name
 FROM Employee
 WHERE id IN (
-    SELECT managerId
-    FROM Employee
-    WHERE managerId IS NOT NULL
-    GROUP BY managerId
-    HAVING COUNT(id) >= 5
+    Select managerId
+    From Employee 
+    Where managerId IS NOT NULL
+    having Count(id) >= 5
 );
