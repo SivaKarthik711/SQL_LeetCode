f3a66254330452bf7aa71f75335c0cddbbae803d
Lakshmi Siva Karthik Padala <siva711karthik@gmail.com>
2025-06-22 23:26:40 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
 SELECT
     Employees.employee_id,
-    CASE WHEN Employees.employee_id %2 !=0 AND Employees.name NOT LIKE 'M%' THEN Employees.salary ELSE 0 END AS bonus
+    CASE WHEN Employees.employee_id % 2 = 1  AND Employees.name NOT LIKE 'M%' THEN salary ELSE 0 END AS bonus
 FROM
     Employees
-ORDER BY
-    Employees.employee_id
